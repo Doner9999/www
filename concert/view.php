7212be3a7211783be29567b25220037b99206cc6
nyeok <nyeok96>
2022-11-29 18:24:42 +0900

    댓글창 추가 업데이트

--- concert/view.php
+++ concert/view.php
@@ -76,6 +76,16 @@
 <link rel="stylesheet" href="sub4common.css">
 <link rel="stylesheet" href="concert.css">
 <script>
+	function check_input()
+	{
+		if (!document.ripple_form.ripple_content.value)
+		{
+			alert("내용을 입력하세요!");    
+			document.ripple_form.ripple_content.focus();
+			return;
+		}
+		document.ripple_form.submit();
+    }
     function del(href) 
     {
         if(confirm("한번 삭제한 자료는 복구할 방법이 없습니다.\n\n정말 삭제하시겠습니까?")) {
@@ -135,6 +145,50 @@
 ?>
 			<?= $item_content ?>
 		</div>
+
+		<div id="ripple">
+		<form  name="ripple_form" method="post" action="insert_ripple.php?table=<?=$table?>&num=<?=$item_num?>">  
+			<div id="ripple_box">
+				<div id="ripple_box1"><span><?= $usernick?></span></div>
+				<div id="ripple_box2"><textarea rows="5" cols="65" name="ripple_content"></textarea>
+				</div>
+				<div id="ripple_box3"><a href="#" onclick="check_input()">쓰기</a></div>
+			</div>
+			</form>
+<?
+	    $sql = "select * from free_ripple where parent='$item_num'";
+	    $ripple_result = mysql_query($sql);
+
+		while ($row_ripple = mysql_fetch_array($ripple_result))
+		{
+			$ripple_num     = $row_ripple[num];
+			$ripple_id      = $row_ripple[id];
+			$ripple_nick    = $row_ripple[nick];
+			$ripple_content = str_replace("\n", "<br>", $row_ripple[content]);
+			$ripple_content = str_replace(" ", "&nbsp;", $ripple_content);
+			$ripple_date    = $row_ripple[regist_day];
+?>
+			<div id="ripple_writer_title">
+			<ul>
+			<li id="writer_title1"><?=$ripple_nick?></li>
+			<li id="writer_title3"> 
+		      <? 
+					if($userid=="admin" || $userid==$ripple_id)
+			          echo "<a href='delete_ripple.php?table=$table&num=$item_num&ripple_num=$ripple_num'></a>"; 
+			  ?>
+			</li>
+			</ul>
+			</div>
+			<div id="ripple_content"><?=$ripple_content?></div>
+			<div id="writer_title2"><?=$ripple_date?></div>
+			
+			<div class="hor_line_ripple"></div>
+<?
+		}
+?>			
+			
+		</div> <!-- end of ripple -->
+		
 
 		<div id="view_button">
 				<a href="list.php?table=<?=$table?>&page=<?=$page?>">목록</a>&nbsp;
